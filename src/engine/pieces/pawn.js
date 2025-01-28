3bb0b48c5d760edfc27206a70a6ab32308767c42
Laila Khan <lailatariqkhan@gmail.com>
2025-01-28 14:26:06 +0000

    test Red 2 passed

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,10 +9,21 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+        if (this.player === Player.WHITE){
+            if(location.row>1){
+            return [Square.at(location.row+1,location.col)];//Square.at(location.row+1, location.col)
+            }
+            else{
+                return [Square.at(location.row+1,location.col), Square.at(location.row+2,location.col)];
+            }
+        }
+        else {  
+            if(location.row<6)  {
+            return [Square.at(location.row - 1, location.col)];
+            }
+            else{
+                return [Square.at(location.row-1, location.col),Square.at(location.row-2,location.col)];
+            }
+        }
         }
     }
-}
