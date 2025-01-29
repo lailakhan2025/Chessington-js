8afaab1c4fc09876d88ddffea0b0edd32b9c9938
Laila Khan <lailatariqkhan@gmail.com>
2025-01-29 11:11:28 +0000

    Red 8 passed

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,21 +9,66 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE){
-            if(location.row>1){
-            return [Square.at(location.row+1,location.col)];//Square.at(location.row+1, location.col)
+        let firstmove = false;
+        let direction =0;
+        if((this.player===Player.WHITE&&location.row===1) || (this.player===Player.BLACK&&location.row===6)){
+            firstmove = true;
+        }
+        this.player===Player.WHITE? direction=1:direction=-1;
+
+            if(!board.getPiece(Square.at(location.row+(1*direction),location.col))){
+                if(!firstmove){
+                return [Square.at(location.row+(1*direction),location.col)];}
+                else{
+                    if(!board.getPiece(Square.at(location.row+(2*direction),location.col))){
+                        return [Square.at(location.row+(1*direction),location.col),Square.at(location.row+(2*direction),location.col)];
+                    }
+                    else{
+                        return [Square.at(location.row+(1*direction),location.col)];}
+                    }
             }
             else{
-                return [Square.at(location.row+1,location.col), Square.at(location.row+2,location.col)];
+                return [];           
+            }
+            
+            }
+        }
+
+
+
+
+
+       /* if (this.player === Player.WHITE){
+            if(location.row===1) {
+                if(board.getPiece(Square.at(location.row+2,location.col))===undefined){
+                    return [Square.at(location.row+2,location.col)];
+                }
+                else if(board.getPiece(Square.at(location.row+1,location.col))===undefined){
+                return [Square.at(location.row+1,location.col)];
+                }
+            }
+            else{
+                if(board.getPiece(Square.at(location.row+1,location.col))===undefined){
+                    return [Square.at(location.row+1,location.col)];
+                }
             }
         }
         else {  
-            if(location.row<6)  {
-            return [Square.at(location.row - 1, location.col)];
+            if(location.row===6) {
+                if(board.getPiece(Square.at(location.row-2,location.col))===undefined){
+                    return [Square.at(location.row-2,location.col)];
+                }
+                else if(board.getPiece(Square.at(location.row-1,location.col))===undefined){
+                return [Square.at(location.row-1,location.col)];
+                }
             }
             else{
-                return [Square.at(location.row-1, location.col),Square.at(location.row-2,location.col)];
+                if(board.getPiece(Square.at(location.row-1,location.col)===undefined)){
+                    return [Square.at(location.row-1,location.col)];
+                }
+
             }
         }
-        }
+    
     }
+}*/
