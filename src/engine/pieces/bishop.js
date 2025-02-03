c57d03551f4ae46e206ab19ba0a6feedf4a4654d
Laila Tariq Khan <lailakhan@192.168.1.149>
2025-02-03 21:26:51 +0000

    passed red-4

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,75 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Bishop extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    const moves = [];
+
+    for (
+      let row = location.row - 1, col = location.col - 1;
+      row >= 0 && col >= 0;
+      row--, col--
+    ) {
+      const piece = board.getPiece(Square.at(row, col));
+      if (piece) {
+        if (piece.player !== this.player) {
+          moves.push(Square.at(row, col));
+        }
+        break;
+      }
+      moves.push(Square.at(row, col));
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+    for (
+      let row = location.row - 1, col = location.col + 1;
+      row >= 0 && col < 8;
+      row--, col++
+    ) {
+      const piece = board.getPiece(Square.at(row, col));
+      if (piece) {
+        if (piece.player !== this.player) {
+          moves.push(Square.at(row, col));
+        }
+        break;
+      }
+      moves.push(Square.at(row, col));
     }
+
+    for (
+      let row = location.row + 1, col = location.col - 1;
+      row < 8 && col >= 0;
+      row++, col--
+    ) {
+      const piece = board.getPiece(Square.at(row, col));
+      if (piece) {
+        if (piece.player !== this.player) {
+          moves.push(Square.at(row, col));
+        }
+        break;
+      }
+      moves.push(Square.at(row, col));
+    }
+
+    for (
+      let row = location.row + 1, col = location.col + 1;
+      row < 8 && col < 8;
+      row++, col++
+    ) {
+      const piece = board.getPiece(Square.at(row, col));
+      if (piece) {
+        if (piece.player !== this.player) {
+          moves.push(Square.at(row, col));
+        }
+        break;
+      }
+      moves.push(Square.at(row, col));
+    }
+
+    return moves;
+  }
 }
