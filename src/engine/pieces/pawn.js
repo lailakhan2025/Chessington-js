9d44fb1d7755911572b929bf23d40a951748ac50
Laila Tariq Khan <lailakhan@192.168.1.149>
2025-02-03 20:28:52 +0000

    passed Red 9

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,61 +1,80 @@
-import Player from '../player';
-import Square from '../square';
-import King from './king';
-import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
+import King from "./king";
+import Piece from "./piece";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        let firstmove = false;
-        let direction =0;
-        if((this.player===Player.WHITE&&location.row===1) || (this.player===Player.BLACK&&location.row===6)){
-            firstmove = true;
-        }
-        this.player===Player.WHITE? direction=1:direction=-1;
-
-        
-      if(location.row+(1*direction)>=0&&location.row+(1*direction)<8){  
-            if(board.getPiece(Square.at(location.row+(1*direction),location.col+1)) && board.getPiece(Square.at(location.row+(1*direction),location.col+1)).player!=this.player&& !(board.getPiece(Square.at(location.row+(1*direction),location.col+1)) instanceof King)){
-                  return [Square.at(location.row+(1*direction),location.col+1)];    
-            }
-            else if (board.getPiece(Square.at(location.row+(1*direction),location.col-1))&&board.getPiece(Square.at(location.row+(1*direction),location.col-1)).player!=this.player && !(board.getPiece(Square.at(location.row+(1*direction),location.col-1))instanceof King))
-            { 
-                return [Square.at(location.row+(1*direction),location.col-1)];
-            }
-            else{
-                    if(!board.getPiece(Square.at(location.row+(1*direction),location.col))){
-                                if(!firstmove){
-                                return [Square.at(location.row+(1*direction),location.col)];
-                                }
-                                else{
-                                        if(!board.getPiece(Square.at(location.row+(2*direction),location.col))){
-                                                return [Square.at(location.row+(1*direction),location.col),Square.at(location.row+(2*direction),location.col)];
-                                        }
-                                        else{
-                                                return [Square.at(location.row+(1*direction),location.col)];
-                                            }
-                                    }
-                    }
-                    else{
-                            return [];          
-                        }
-                }
-        }
-        else{
-            return [];
-            }
-        
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    let firstmove = false;
+    let direction = 0;
+    if (
+      (this.player === Player.WHITE && location.row === 1) ||
+      (this.player === Player.BLACK && location.row === 6)
+    ) {
+      firstmove = true;
     }
+    this.player === Player.WHITE ? (direction = 1) : (direction = -1);
+
+    if (location.row + 1 * direction >= 0 && location.row + 1 * direction < 8) {
+      if (
+        board.getPiece(
+          Square.at(location.row + 1 * direction, location.col + 1)
+        ) &&
+        board.getPiece(
+          Square.at(location.row + 1 * direction, location.col + 1)
+        ).player != this.player &&
+        !(
+          board.getPiece(
+            Square.at(location.row + 1 * direction, location.col + 1)
+          ) instanceof King
+        )
+      ) {
+        return [Square.at(location.row + 1 * direction, location.col + 1)];
+      } else if (
+        board.getPiece(
+          Square.at(location.row + 1 * direction, location.col - 1)
+        ) &&
+        board.getPiece(
+          Square.at(location.row + 1 * direction, location.col - 1)
+        ).player != this.player &&
+        !(
+          board.getPiece(
+            Square.at(location.row + 1 * direction, location.col - 1)
+          ) instanceof King
+        )
+      ) {
+        return [Square.at(location.row + 1 * direction, location.col - 1)];
+      } else {
+        if (
+          !board.getPiece(Square.at(location.row + 1 * direction, location.col))
+        ) {
+          if (!firstmove) {
+            return [Square.at(location.row + 1 * direction, location.col)];
+          } else {
+            if (
+              !board.getPiece(
+                Square.at(location.row + 2 * direction, location.col)
+              )
+            ) {
+              return [
+                Square.at(location.row + 1 * direction, location.col),
+                Square.at(location.row + 2 * direction, location.col),
+              ];
+            } else {
+              return [Square.at(location.row + 1 * direction, location.col)];
+            }
+          }
+        } else {
+          return [];
+        }
+      }
+    } else {
+      return [];
+    }
+  }
 }
-
-
-
-
-
-
-   
-        
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,23 +1,42 @@
-import Piece from './piece';
-import Square from '../square';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Rook extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    const moves = [];
+
+    for (let row = location.row - 1; row >= 0; row--) {
+      if (board.getPiece(Square.at(row, location.col))) {
+        break;
+      }
+      moves.push(Square.at(row, location.col));
     }
 
-    getAvailableMoves(board) {
-        let location = board.findPiece(this);
-        const moves=[];
-            for (let row=0; row<8; row++){
-                if(row!==location.row)
-                moves.push(Square.at(row,location.col));
-            }
-            for(let col=0;col<8;col++){
-                if(col!==location.col)
-                moves.push(Square.at(location.row,col));
-            }
-        return moves;
+    for (let row = location.row + 1; row < 8; row++) {
+      if (board.getPiece(Square.at(row, location.col))) {
+        break;
+      }
+      moves.push(Square.at(row, location.col));
     }
+
+    for (let col = location.col - 1; col >= 0; col--) {
+      if (board.getPiece(Square.at(location.row, col))) {
+        break;
+      }
+      moves.push(Square.at(location.row, col));
+    }
+    for (let col = location.col + 1; col < 8; col++) {
+      if (board.getPiece(Square.at(location.row, col))) {
+        break;
+      }
+      moves.push(Square.at(location.row, col));
+    }
+
+    return moves;
+  }
 }
-
