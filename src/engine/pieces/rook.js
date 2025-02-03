b30a4aaaaf5be9c16310241fd2bdfdc9b9687f3b
Laila Tariq Khan <lailakhan@192.168.1.149>
2025-02-03 21:15:22 +0000

    red 9 passed

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,5 +1,6 @@
 import Piece from "./piece";
 import Square from "../square";
+import King from "./king";
 
 export default class Rook extends Piece {
   constructor(player) {
@@ -11,32 +12,75 @@ export default class Rook extends Piece {
     const moves = [];
 
     for (let row = location.row - 1; row >= 0; row--) {
-      if (board.getPiece(Square.at(row, location.col))) {
+      if (
+        (board.getPiece(Square.at(row, location.col)) &&
+          board.getPiece(Square.at(row, location.col)).player == this.player) ||
+        board.getPiece(Square.at(row, location.col)) instanceof King
+      ) {
         break;
+      } else if (
+        board.getPiece(Square.at(row, location.col)) &&
+        board.getPiece(Square.at(row, location.col)).player != this.player
+      ) {
+        moves.push(Square.at(row, location.col));
+        break;
+      } else {
+        moves.push(Square.at(row, location.col));
       }
-      moves.push(Square.at(row, location.col));
     }
 
     for (let row = location.row + 1; row < 8; row++) {
-      if (board.getPiece(Square.at(row, location.col))) {
+      if (
+        (board.getPiece(Square.at(row, location.col)) &&
+          board.getPiece(Square.at(row, location.col)).player == this.player) ||
+        board.getPiece(Square.at(row, location.col)) instanceof King
+      ) {
         break;
+      } else if (
+        board.getPiece(Square.at(row, location.col)) &&
+        board.getPiece(Square.at(row, location.col)).player != this.player
+      ) {
+        moves.push(Square.at(row, location.col));
+        break;
+      } else {
+        moves.push(Square.at(row, location.col));
       }
-      moves.push(Square.at(row, location.col));
     }
 
     for (let col = location.col - 1; col >= 0; col--) {
-      if (board.getPiece(Square.at(location.row, col))) {
+      if (
+        (board.getPiece(Square.at(location.row, col)) &&
+          board.getPiece(Square.at(location.row, col)).player == this.player) ||
+        board.getPiece(Square.at(location.row, col)) instanceof King
+      ) {
         break;
+      } else if (
+        board.getPiece(Square.at(location.row, col)) &&
+        board.getPiece(Square.at(location.row, col)).player != this.player
+      ) {
+        moves.push(Square.at(location.row, col));
+        break;
+      } else {
+        moves.push(Square.at(location.row, col));
       }
-      moves.push(Square.at(location.row, col));
     }
     for (let col = location.col + 1; col < 8; col++) {
-      if (board.getPiece(Square.at(location.row, col))) {
+      if (
+        (board.getPiece(Square.at(location.row, col)) &&
+          board.getPiece(Square.at(location.row, col)).player == this.player) ||
+        board.getPiece(Square.at(location.row, col)) instanceof King
+      ) {
         break;
+      } else if (
+        board.getPiece(Square.at(location.row, col)) &&
+        board.getPiece(Square.at(location.row, col)).player != this.player
+      ) {
+        moves.push(Square.at(location.row, col));
+        break;
+      } else {
+        moves.push(Square.at(location.row, col));
       }
-      moves.push(Square.at(location.row, col));
     }
-
     return moves;
   }
 }
